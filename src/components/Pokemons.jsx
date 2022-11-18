5256221e5ba540c29e8e37c86fd974a5d49311c7
Katia Jauregui <katiajauregui@gmail.com>
2022-11-17 21:14:07 -0600

    add api

--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 const Pokemons = () => {
 
-    const userName = useSelector(state => state.username)
+    const userName = useSelector(state => state.username);
 
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/')
+            // Al hacer la paginaciòn traer todos los pokemones, offset 0 limit 1154
+            .then(res => setPokemons(res.data.results))
+    }, [])
+
+    console.log(pokemons);
     return (
         <div>
             <h1>Pokemons</h1>
             <p>Welcome {userName}!</p>
+            {pokemons.map(pokemon => (
+                <li key={pokemon.name}>{pokemon.url}</li>
+            ))}
         </div>
     );
 };
