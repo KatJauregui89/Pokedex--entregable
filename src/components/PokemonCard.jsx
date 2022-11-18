57f5d958f74ac75ce84b8d660667f46d60025c96
Katia Jauregui <katiajauregui@gmail.com>
2022-11-17 21:56:33 -0600

    add Link

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const PokemonCard = ({ url }) => {
     const [pokemon, setPokemon] = useState({})
@@ -9,13 +10,13 @@ const PokemonCard = ({ url }) => {
             .then(res => setPokemon(res.data))
     }, [])
 
-
+    console.log(pokemon);
     return (
-        <div className='pokecard'>
+        <Link to={`/pokemons/${pokemon.id}`} className='pokecard'>
             <h3>{pokemon.name}</h3>
             <br />
-            <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
-        </div>
+            <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
+        </Link>
     );
 };
 
