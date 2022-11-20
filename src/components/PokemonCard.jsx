ecbca37d627979217dc8ba15882e1ba5392a2507
Ricardo Salazar <axel.111yo@gmail.com>
2022-11-19 23:00:48 -0600

    Pokecards and pagination of pokecrads

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -9,14 +9,37 @@ const PokemonCard = ({ url }) => {
         axios.get(url)
             .then(res => setPokemon(res.data))
     }, [])
+    let color = ''
+    const type = pokemon.types?.[0].type.name
+    type ==='grass'?color='rgb(112, 204, 177)':
+    type === 'fire'?color = 'rgb(235, 117, 113)':
+    type === 'water'?color = 'rgb(125, 183, 244)':
+    type === 'bug'?color = 'rgb(142, 231, 124)':
+    type === 'normal'?color = 'rgb(100, 74, 81)':
+    type === 'poison'?color = 'rgb(134, 90, 156)':
+    type === 'fighting'?color = 'rgb(141, 67, 45)':
+    type === 'flying'?color = 'rgb(78, 102, 121)':
+    type === 'rock'?color = 'rgb(253, 234, 96)':
+    type === 'ghost'?color = 'rgb(48, 53, 102)':
+    type ==='steel'?color='rgb(97, 114, 108)':
+    type === 'electric'?color = 'rgb(227, 222, 85)':
+    type === 'psychic'?color = 'rgb(149, 209, 240)':
+    type === 'bug'?color = 'rgb(85, 136, 146)':
+    type === 'dark'?color = 'rgb(4, 7, 6)':
+    type === 'fairy'?color = 'rgb(140, 39, 68)':
+    type === 'unknown'?color = 'red':
+    type === 'shadow'?color = 'red':
+    color = 'red'
     return (
-        <Link to={`/pokemons/${pokemon.id}`} className='pokecard'>
-            <h3>{pokemon.name}</h3>
-            <div><b>Types: </b>{pokemon.types?.map((type)=> type.type.name + ' ')}</div>
-            <div><b>hp: </b>{pokemon.stats?.[0].base_stat}</div>
-            <div><b>attack: </b>{pokemon.stats?.[1].base_stat}</div>
-            <div><b>defense: </b>{pokemon.stats?.[2].base_stat}</div>
-            <div><b>speed: </b>{pokemon.stats?.[5].base_stat}</div>
+        <Link to={`/pokemons/${pokemon.id}`} className='pokecard' style={{backgroundColor:color}}>
+            <div className='card-description'>
+                <h3>{pokemon.name}</h3>
+                <div><b>Types: </b>{pokemon.types?.map((type) => type.type.name + ' ')}</div>
+                <div><b>hp: </b>{pokemon.stats?.[0].base_stat}</div>
+                <div><b>attack: </b>{pokemon.stats?.[1].base_stat}</div>
+                <div><b>defense: </b>{pokemon.stats?.[2].base_stat}</div>
+                <div><b>speed: </b>{pokemon.stats?.[5].base_stat}</div>
+            </div>
             <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
         </Link>
     );
--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -6,27 +6,51 @@ import PokemonCard from './PokemonCard';
 const Pokemons = () => {
 
     const userName = useSelector(state => state.username);
-
     const [pokemons, setPokemons] = useState([]);
+    const [page, setPage] = useState(1)
+    const pokemonsPerPage = 16
+    const lastIndex = page * pokemonsPerPage
+    const totalPages = Math.ceil(pokemons.length / pokemonsPerPage)
+
+    const firstIndex = lastIndex - pokemonsPerPage
+
+    const pokemonPaginated = pokemons.slice(firstIndex, lastIndex)
+
+    const pages = []
 
     useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon/')
-            // Al hacer la paginaciòn traer todos los pokemones, offset 0 limit 1154
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154')
             .then(res => setPokemons(res.data.results))
     }, [])
 
+    for (let i = 1; i <= totalPages; i++) {
+        pages.push(i)
+    }
     return (
         <div>
             <h1>Pokemons</h1>
             <p>Welcome {userName}!</p>
-            <ul>
-                {pokemons.map(pokemon => (
+            <ul className='pokemons'>
+                {pokemonPaginated.map(pokemon => (
                     <li key={pokemon.name}>
                         <PokemonCard url={pokemon.url} />
                     </li>
                     // <li key={pokemon.name}>{pokemon.url}</li>
                 ))}
             </ul>
+            <button
+                onClick={() => setPage(page - 1)}
+                disabled={page === 1}
+            >adios</button>
+            <div>
+                {pages.map((number) => (
+                    <button key={number} onClick={() => setPage(number)}>{number}</button>
+                ))}
+            </div>
+            <button
+                onClick={() => setPage(page + 1)}
+                disabled={page === totalPages}
+            >hola</button>
         </div>
     );
 };
