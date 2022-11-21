801767551fc50cebae2bc7a1e9120f4789eca965
Ricardo Salazar <axel.111yo@gmail.com>
2022-11-21 00:52:46 -0600

    started pokemon details

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -14,15 +14,63 @@ const PokemonDetail = () => {
 
     return (
         <div className='pokemon-full-description'>
-            <div>
-                <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
-                <div>
+            <div className='first-grid'>
+                <img src={pokemon.sprites?.other.dream_world.front_default} alt="" className='main-picture' />
+                <div className='W-and-H'>
                     <b>{pokemon.weight}</b>
                     <b>{pokemon.height}</b>
                 </div>
-                <div>
+                <div className='Name-and-id'>
                     <b>{pokemon.name}</b>
                     <b>{pokemon.id}</b>
+                </div>
+            </div>
+            <div>
+                <div>Types</div>
+                <div>
+                    <ul>
+                        {
+                            pokemon.types?.map(type => (
+                                <div key={type.slot}>{type.type.name}</div>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div>Abilities</div>
+                <div>
+                    <ul>
+                        {
+                            pokemon.abilities?.map(ability => (
+                                <div key={ability.slot}>{ability.ability?.name}</div>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div>Stats Base</div>
+                <div>
+                    <ul>
+                        {
+                            pokemon.stats?.map(stat => (
+                                <div key={stat.stat.name}>{stat.stat.name}{' '}{stat.base_stat}</div>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div>Movements</div>
+                <div>
+                    <ul>
+                        {
+                            pokemon.moves?.map(move=>(
+                                <div key={move.move.name}>{move.move.name}</div>
+                            ))
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
