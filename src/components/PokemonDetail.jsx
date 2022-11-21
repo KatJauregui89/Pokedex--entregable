aebe3c94b2973a90ce44b7830761dfdd31e4d796
Ricardo Salazar <axel.111yo@gmail.com>
2022-11-21 14:48:36 -0600

    Added buttons to pokemons route and input to search pokemons(need functions)

--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -13,64 +13,78 @@ const PokemonDetail = () => {
     }, [])
 
     return (
-        <div className='pokemon-full-description'>
-            <div className='first-grid'>
-                <img src={pokemon.sprites?.other.dream_world.front_default} alt="" className='main-picture' />
-                <div className='W-and-H'>
-                    <b>{pokemon.weight}</b>
-                    <b>{pokemon.height}</b>
-                </div>
-                <div className='Name-and-id'>
-                    <h3>{pokemon.name}</h3>
-                    <b>{pokemon.id}</b>
-                </div>
+        <div className='pokemon-details'>
+            <div className='header'>
+                <img
+                    src="https://logos-marcas.com/wp-content/uploads/2020/05/Pokemon-Logo.png"
+                    alt=""
+                    className='pokemon-image'
+                />
             </div>
-            <div className='second-grid'>
-                <h3>Types</h3>
-                <div>
-                    <ul className='types-full-description'>
-                        {
-                            pokemon.types?.map(type => (
-                                <div key={type.slot}>{type.type.name}</div>
-                            ))
-                        }
-                    </ul>
+            <div className='pokemon-full-description'>
+                <div className='first-grid'>
+                    <img src={pokemon.sprites?.other.dream_world.front_default} alt="" className='main-picture' />
+                    <div className='W-and-H'>
+                        <div>
+                            <b>{pokemon.weight}</b><br />
+                            <span>weight</span>
+                        </div>
+                        <div>
+                            <b>{pokemon.height}</b><br />
+                            <span>height</span>
+                        </div>
+                    </div>
+                    <div className='Name-and-id'>
+                        <h3>{pokemon.name}</h3>
+                        <b>{pokemon.id}</b>
+                    </div>
                 </div>
-            </div>
-            <div className='third-grid'>
-                <h3>Abilities</h3>
-                <div>
-                    <ul className='abilities-full-description'>
-                        {
-                            pokemon.abilities?.map(ability => (
-                                <div key={ability.slot}>{ability.ability?.name}</div>
-                            ))
-                        }
-                    </ul>
+                <div className='second-grid'>
+                    <h3>Types</h3>
+                    <div>
+                        <ul className='types-full-description'>
+                            {
+                                pokemon.types?.map(type => (
+                                    <div key={type.slot}>{type.type.name}</div>
+                                ))
+                            }
+                        </ul>
+                    </div>
                 </div>
-            </div>
-            <div className='fourth-grid'>
-                <h3>Stats Base</h3>
-                <div>
-                    <ul>
+                <div className='third-grid'>
+                    <h3>Abilities</h3>
+                    <div>
+                        <ul className='abilities-full-description'>
+                            {
+                                pokemon.abilities?.map(ability => (
+                                    <div key={ability.slot}>{ability.ability?.name}</div>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
+                <div className='fourth-grid'>
+                    <h3>Stats Base</h3>
+                    <div>
                         {
                             pokemon.stats?.map(stat => (
                                 <div key={stat.stat.name}>{stat.stat.name}{' '}{stat.base_stat}</div>
                             ))
                         }
-                    </ul>
+
+                    </div>
                 </div>
-            </div>
-            <div className='fifth-grid'>
-                <h3>Movements</h3>
-                <div>
-                    <ul>
+                <div className='fifth-grid'>
+                    <h3>Movements</h3>
+                    <div>
                         {
-                            pokemon.moves?.map(move=>(
-                                <div key={move.move.name}>{move.move.name}</div>
+                            pokemon.moves?.map(move => (
+                                <div key={move.move.name} className='move'>
+                                    {move.move.name}
+                                </div>
                             ))
                         }
-                    </ul>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Pokemons.jsx
+++ src/components/Pokemons.jsx
@@ -8,7 +8,9 @@ const Pokemons = () => {
     const userName = useSelector(state => state.username);
     const [pokemons, setPokemons] = useState([]);
     const [page, setPage] = useState(1)
+    const [writeName, setWriteName] = useState(false)
     const [filter, setFilter] = useState('')
+    const [pokemonName, setPokemonName] = useState('')
 
     //pagination
     const pokemonsPerPage = 16
@@ -18,14 +20,18 @@ const Pokemons = () => {
     const pokemonPaginated = pokemons.slice(firstIndex, lastIndex)
     const pages = []
 
+
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154')
             .then(res => setPokemons(res.data.results))
+
     }, [])
 
     for (let i = 1; i <= totalPages; i++) {
         pages.push(i)
     }
+
+    const buttonsPaginated = pages.slice(page - 1, page+10)
 
     return (
         <div className='pokedex-container'>
@@ -35,12 +41,11 @@ const Pokemons = () => {
             <div className='pokedex-forms'>
                 <div className='check-container'>
                     <label htmlFor="check-box" className='lbl-switch' id='prueba'></label>
-                    <input type="checkbox" id='check-box'/>
+                    <input type="checkbox" id='check-box' onChange={()=>setWriteName(!writeName)}/>
                 </div>
 
                 <section>
-                    
-                    <select id='types' onChange={(e)=>{setFilter(e.target.value)}}>
+                    <select className={writeName?'hide':'visible'} id='types' onChange={(e) => { setFilter(e.target.value) }}>
                         <option value="">All pokemons</option>
                         <option value="Normal" >Normal</option>
                         <option value="fighting" >fighting</option>
@@ -64,31 +69,49 @@ const Pokemons = () => {
                         <option value="shadow" >shadow</option>
 
                     </select>
+                    <div className={!writeName?'hide':'visible'}>
+                        <label htmlFor="search-pokemon-name"></label>
+                        <input type="text" id='search-pokemon-name' value={pokemonName}
+                        onChange={e=>setPokemonName(e.target.value)}/>
+                    </div>
                 </section>
             </div>
 
 
             <ul className='pokemons'>
                 {pokemonPaginated.map(pokemon => (
+                    
                     <li key={pokemon.name}>
-                        <PokemonCard url={pokemon.url}/>
+                        
+                        <PokemonCard url={pokemon.url} />
+
                     </li>
                     // <li key={pokemon.name}>{pokemon.url}</li>
                 ))}
             </ul>
-            <button
-                onClick={() => setPage(page - 1)}
-                disabled={page === 1}
-            >prev</button>
-            <div>
-                {pages.map((number) => (
-                    <button key={number} onClick={() => setPage(number)}>{number}</button>
-                ))}
+            <div className='buttons-page main'>
+                <button
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
+                    className='button-page'
+                    >prev</button>
+                <div className='buttons-page'>
+                    {buttonsPaginated.map((number) => (
+                        <button
+                            key={number}
+                            onClick={() => { setPage(number) }}
+                            className='button-page'
+                        >{number}</button>
+                    ))}
+                </div>
+                <button
+                    onClick={() => {
+                        setPage(page + 1)
+                    }}
+                    disabled={page === totalPages}
+                    className='button-page'
+                >next</button>
             </div>
-            <button
-                onClick={() => setPage(page + 1)}
-                disabled={page === totalPages}
-            >next</button>
         </div>
     );
 };
